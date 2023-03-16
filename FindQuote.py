04fd6ac2ea2ca14176b2d17f192808512f1e1f6f
Christos Koumpis <56029580+Button99@users.noreply.github.com>
2023-03-16 11:18:56 +0200

    Used My quote API

--- FindQuote.py
+++ FindQuote.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
+
 def findQuote():
-  try:
-    res = requests.get("https://api.quotable.io/random")
-    jsonData = json.loads(res.text)
-    msgRes = jsonData["content"] + "\n-" + jsonData["author"]
-    return msgRes
-  except:
-    return "Wait a lil bit :/"    
+    try:
+        res = requests.get("https://quoteapi.button99.repl.co/").json()
+        msgRes = res["quote"] + "\n-" + res["name"]
+        return msgRes
+    except:
+        return "Wait a lil bit :/"
